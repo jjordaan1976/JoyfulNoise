f41b4e3261825cb705499b01dec6c5508f3e5348
Johan Jordaan <johan.jordaan@prpsolutions.com>
2026-06-10 20:07:57 +0200

    Add Authentication

--- JwtService/IActivationTokenService.cs
+++
@@ -1,7 +0,0 @@
-namespace JwtService;
-
-public interface IActivationTokenService
-{
-    string CreateToken(string email, int companyId, string secret);
-    (string Email, int CompanyId) ValidateToken(string token, string secret);
-}
--- JwtService/IJwtTokenService.cs
+++
@@ -1,8 +0,0 @@
-namespace JwtService;
-
-public interface IJwtTokenService
-{
-    string CreateToken(string userId, string secret, DateTime expiration);
-    string ValidateToken(string token, string secret);
-    string RotateToken(string token, string secret, DateTime newExpiration);
-}
